69ef41484af69cec6abb03a667eb957e909c200f
yitznewton <yitznewton@hotmail.com>
2014-07-08 18:25:27 -0400

    improve browser code
    change project name in HTML
    
    reinstate project_id filter for initialization

--- index.js
+++ index.js
@@ -2,40 +2,47 @@ var socket = io();
 var projectId = 1;
 var noteTextarea = document.querySelector('div[data-note-id="1"] textarea');
 
-socket.on('note_updated', function(msg) {
-    console.log('message received', msg);
-    textareas[msg.id].value = msg.text;
-});
-
-socket.emit('get_notes', {project_id: projectId});
-
-socket.on('project_notes', function(msg) {
-    socket.removeAllListeners('project_notes');
-    initializeNotes(msg.notes);
-});
-
 var noteContainer = document.getElementById('notes');
 var textareas = {};
 
-var initializeNotes = function(notes) {
+var addNote = function(note) {
+    var textarea = document.createElement('TEXTAREA');
+    textarea.dataset.noteId = note.id;
+    textarea.value = note.text;
+    noteContainer.appendChild(textarea);
+    textareas[note.id] = textarea;
+
+    textarea.addEventListener('blur', (function(note) {
+        return function() {
+            console.log('sending update to server for note ' + note.id + ': ' + this.value);
+            socket.emit('update_note', {
+                note_id: note.id,
+                project_id: projectId,
+                text: this.value
+            });
+        };
+    })(note));
+}
+
+var initializeNotes = function(msg) {
+    if (msg.project_id != projectId) {
+        return;
+    }
+
+    console.log('loading notes from server');
+    socket.removeListener('project_notes', initializeNotes);
+
+    var notes = msg.notes;
     for (var i in notes) {
         var note = notes[i];
-        var textarea = document.createElement('TEXTAREA');
-        textarea.dataset.noteId = note.id;
-        textarea.value = note.text;
-        noteContainer.appendChild(textarea);
-        textareas[note.id] = textarea;
-
-        textarea.addEventListener('blur', (function(note) {
-            return function() {
-                console.log('updating note ' + note.id + ': ' + this.value);
-                socket.emit('update_note', {
-                    note_id: note.id,
-                    project_id: projectId,
-                    text: this.value
-                });
-            };
-        })(note));
+        addNote(notes[i]);
     }
 };
 
+socket.on('note_updated', function(msg) {
+    console.log('updating note from server', msg);
+    textareas[msg.id].value = msg.text;
+});
+
+socket.on('project_notes', initializeNotes);
+socket.emit('get_notes', {project_id: projectId});
