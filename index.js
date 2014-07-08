3b381923d7ea6ba072608e6b9ee3fdfcbe8e3f3e
yitznewton <yitznewton@hotmail.com>
2014-07-08 16:50:58 -0400

    refactor DOM build

--- index.js
+++ index.js
@@ -3,14 +3,34 @@ var noteTextarea = document.querySelector('div[data-note-id="1"] textarea');
 
 socket.on('note_updated', function(msg) {
     console.log('message received', msg);
-    noteTextarea.value = msg.text;
+    textareas[msg.id].value = msg.text;
 });
 
-noteTextarea.addEventListener('blur', function() {
-    console.log('updating note: ' + this.value);
-    socket.emit('update_note', {
-        note_id: 1,
-        project_id: 1,
-        text: this.value
+var noteContainer = document.getElementById('notes');
+var textareas = {};
+
+var notes = [
+    {
+        id: 1,
+        text: "jim the bob"
+    }
+];
+
+for (var i in notes) {
+    var note = notes[i];
+    var textarea = document.createElement('TEXTAREA');
+    textarea.dataset.noteId = note.id;
+    textarea.value = note.text;
+    noteContainer.appendChild(textarea);
+    textareas[note.id] = textarea;
+    
+    textarea.addEventListener('blur', function() {
+        console.log('updating note: ' + this.value);
+        socket.emit('update_note', {
+            note_id: note.id,
+            project_id: 1,
+            text: this.value
+        });
     });
-});
+}
+
