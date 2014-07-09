26a8138dcf0867ca17a4560f8c6a6da96d6e7bc7
yitznewton <yitznewton@hotmail.com>
2014-07-09 15:06:17 -0400

    add node cluster

--- socketio_server.js
+++ socketio_server.js
@@ -3,86 +3,101 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var amqp = require('amqplib');
 var when = require('when');
+var cluster = require('cluster');
 
-app.get('/', function(req, rsp) {
-    rsp.sendfile('index.html');
-});
+var numCpus = 2;
 
-app.get('/index.js', function(req, rsp) {
-    rsp.sendfile('index.js');
-});
+if (cluster.isMaster) {
+    // Fork workers.
+    for (var i = 0; i < numCpus; i++) {
+        cluster.fork();
+    }
 
-var jobsQueueName = 'sockbit_work',
-    announceQueueName = 'sockbit_announce',
-    jobsChannel;
+    cluster.on('exit', function(worker, code, signal) {
+        console.log('worker ' + worker.process.pid + ' died');
+    });
+} else {
+    app.get('/', function(req, rsp) {
+        rsp.sendfile('index.html');
+    });
 
-var prepRabbitAnnounceChannel = function(conn) {
-    return conn.createChannel().then(function(channel) {
-        channel.assertExchange(announceQueueName, 'fanout', {durable: false}).then(function() {
-            return channel.assertQueue('', {exclusive: true});
-        }).then(function(queueOk) {
-            announceQueue = queueOk.queue;
-            return channel.bindQueue(announceQueue, announceQueueName, '');
+    app.get('/index.js', function(req, rsp) {
+        rsp.sendfile('index.js');
+    });
+
+    var jobsQueueName = 'sockbit_work',
+        announceQueueName = 'sockbit_announce',
+        jobsChannel;
+
+    var prepRabbitAnnounceChannel = function(conn) {
+        return conn.createChannel().then(function(channel) {
+            channel.assertExchange(announceQueueName, 'fanout', {durable: false}).then(function() {
+                return channel.assertQueue('', {exclusive: true});
+            }).then(function(queueOk) {
+                announceQueue = queueOk.queue;
+                return channel.bindQueue(announceQueue, announceQueueName, '');
+            });
+
+            return channel;
         });
+    };
 
-        return channel;
-    });
-};
+    var forwardJob = function(jobName, socket) {
+        console.log('registering forward of ' + jobName + ' job requests to rabbit');
 
-var forwardJob = function(jobName, socket) {
-    console.log('registering forward of ' + jobName + ' job requests to rabbit');
+        socket.on(jobName, function(message) {
+            var jobString = JSON.stringify([jobName, message]);
+            console.log('forwarding job to rabbit: ' + jobString);
+            console.log('sending job to rabbit');
+            jobsChannel.sendToQueue(jobsQueueName, new Buffer(jobString), {deliveryMode:true});
+        });
+    };
 
-    socket.on(jobName, function(message) {
-        var jobString = JSON.stringify([jobName, message]);
-        console.log('forwarding job to rabbit: ' + jobString);
-        console.log('sending job to rabbit');
-        jobsChannel.sendToQueue(jobsQueueName, new Buffer(jobString), {deliveryMode:true});
-    });
-};
+    amqp.connect('amqp://localhost').then(function(conn) {
+        when(conn.createChannel()).then(function(ch) {
+            jobsChannel = ch;
+            return ch.assertQueue(jobsQueueName, {durable: true});
+        }).then(function() {
+            return conn.createChannel();
+        }).then(function(announceChannel) {
+            announceChannel.assertExchange(announceQueueName, 'fanout', {durable: false});
+            return announceChannel;
+        }).then(function(announceChannel) {
+            var queueOk = announceChannel.assertQueue('', {exclusive: true});
+            return [announceChannel, queueOk];
+        }).then(function(objs) {
+            var announceChannel = objs[0];
+            announceQueue = objs[1].queue;
+            announceChannel.bindQueue(announceQueue, announceQueueName, '');
+            return [announceChannel, announceQueue];
+        }).then(function(objs) {
+            var announceChannel = objs[0];
+            var announceQueue = objs[1];
 
-amqp.connect('amqp://localhost').then(function(conn) {
-    when(conn.createChannel()).then(function(ch) {
-        jobsChannel = ch;
-        return ch.assertQueue(jobsQueueName, {durable: true});
-    }).then(function() {
-        return conn.createChannel();
-    }).then(function(announceChannel) {
-        announceChannel.assertExchange(announceQueueName, 'fanout', {durable: false});
-        return announceChannel;
-    }).then(function(announceChannel) {
-        var queueOk = announceChannel.assertQueue('', {exclusive: true});
-        return [announceChannel, queueOk];
-    }).then(function(objs) {
-        var announceChannel = objs[0];
-        announceQueue = objs[1].queue;
-        announceChannel.bindQueue(announceQueue, announceQueueName, '');
-        return [announceChannel, announceQueue];
-    }).then(function(objs) {
-        var announceChannel = objs[0];
-        var announceQueue = objs[1];
+            announceChannel.consume(announceQueue, function(message) {
+                var update = JSON.parse(message.content);
+                var announcementName = update[0];
+                var data = update[1];
+                console.log('receiving announcement from rabbit: ' + message.content);
+                console.log('sending update to browser clients');
+                io.emit(announcementName, data);
+            }, {noack: true});
+        }).then(function() {
+            console.log('listening for announcements from rabbit');
 
-        announceChannel.consume(announceQueue, function(message) {
-            var update = JSON.parse(message.content);
-            var announcementName = update[0];
-            var data = update[1];
-            console.log('receiving announcement from rabbit: ' + message.content);
-            console.log('sending update to browser clients');
-            io.emit(announcementName, data);
-        }, {noack: true});
-    }).then(function() {
-        console.log('listening for announcements from rabbit');
+            io.on('connection', function(socket) {
+                console.log('a user connected');
 
-        io.on('connection', function(socket) {
-            console.log('a user connected');
-
-            forwardJob('update_note', socket);
-            forwardJob('get_notes', socket);
+                forwardJob('update_note', socket);
+                forwardJob('get_notes', socket);
+            });
         });
     });
-});
 
-var port = process.argv[2];
+    var port = process.argv[2];
 
-http.listen(port, function() {
-    console.log('listening on port ' + port);
-});
+    http.listen(port, function() {
+        console.log('listening on port ' + port);
+    });
+}
+
